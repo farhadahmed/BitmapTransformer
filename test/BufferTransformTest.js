e77634a28c74eb9aea52f027553c5fe9b6943175
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-07-20 11:55:40 -0700

    Create working test and change index.js to invert existing bmp

--- test/BufferTransformTest.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai');
-var readCreateTransform = require('../lib/readCreateTransform.js');
-
-describe('readCreateTransform.js', function() {
-  it('will be the same length as original buffer', function() {
-    expect('../lib/newImage.bmp'.length).to.eql('../bitmaps/palette-bitmap.bmp'.length);
-  });
-});
