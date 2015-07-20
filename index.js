97183bf4fa937c265af136919548deed44aa187e
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-07-20 11:55:25 -0700

    Create working test and change index.js to invert existing bmp

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ var buf2 = fs.readFileSync('./bitmaps/palette-bitmap.bmp');
 
 var secondBuf = new Buffer(buf2.length);
 var counter = 0;
+
+//Creates copy of buf2
 while (counter < buf2.length-1) {
   secondBuf[counter] = buf2.readUInt8(counter);
   counter++;
 }
-
+console.log(secondBuf === buf2);
 //Write secondBuf (a copy of buf2) to the new tempFile
 //tempFile.write(secondBuf);
 //tempFile.end();
@@ -27,17 +29,20 @@ while (counter < buf2.length-1) {
 //console.log(secondBuf.readUInt32BE(10));
 //console.log(255 - secondBuf.readUInt8(1078));
 
-
-////Tells us length of file
-//fs.readFile('./temp.bmp', function(err, data) {
-// console.log(data.length);
-//});
 console.log('secondBuf starts at: ' + secondBuf.readUInt32LE(10))
 
 
-for (var i = 1078; i < buf2.length-1; i++) {
-  secondBuf[i] = 5 + buf2.readUInt8(i);
+//for (var i = 1078; i < buf2.length-1; i++) {
+//  secondBuf.writeUInt8(255 - buf2.readUInt8(i), i);
+//};
+var offset = secondBuf.readUInt32LE(10);
+
+var palette = secondBuf.slice(54, offset);
+for (var j = 0; j < 256 * 4; j++) {
+  //255-readUINt.. changes palette. ',j' says which pixel to change
+  palette.writeUInt8(255 - palette.readUInt8(j), j);
 };
+
 tempFile.write(secondBuf);
 tempFile.end();
 
